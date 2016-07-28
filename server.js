e4b2eb4ce6b3358642a12e457c19ddc1d2f6899f
Carsair <cbanov3@gmail.com>
2016-07-27 22:01:49 -0400

    Remove log.

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  console.log("log");
   res.render('home', {
     name: "Hillary vs Trump"
   });
