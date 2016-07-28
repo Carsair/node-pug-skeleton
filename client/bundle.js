41f7c11a618a477f36e4e73b94fbbd15444d20a2
Carsair <cbanov3@gmail.com>
2016-07-27 21:52:08 -0400

    Initial home page and skeleton.

---
+++ client/bundle.js
@@ -0,0 +1,3 @@
+var $ = require("jquery");
+
+console.log("Hillary vs Trump");
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var pug = require('pug');
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    name: "Hillary vs Trump"
+  });
+});
+
+var PORT = process.env.PORT || 9000
+console.log("Listening on port: ", PORT);
+var server = app.listen(PORT);
